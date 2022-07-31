44e4460e6de7bd22d8f3f0a7fbee267578cc6807
AntonieV <antonie.vietor@gmx.de>
2022-07-31 19:28:42 +0200

    implementation of comparison of two excel files and changes on README

--- README.md
+++ README.md
@@ -1,18 +1,107 @@
 # ExcelDiff
 
-A tool to compare two excel files with annotation of the differences. 
+A tool to compare two excel files with annotation of the differences. The files 
+to be compared must have the same basic structure. That means the labels and the 
+number of their sheets must be the same in both files. 
 
+## Download and Installation for Linux:
+
+### Install or upgrade pip:
+````
 sudo apt install python3-pip
 pip3 install --upgrade pip
-(requires pip>=22.2.1)
+````
+[//]: # (&#40;requires pip>=22.2.1&#41;)
 
+Clone the repo or download the latest release in .zip or .tar.gz format.
 
-pip3 uninstall exceldiff
+### Download and extract repository in .tar.gz format: 
+
+Check if `wget` is installed:
+````
+which wget; echo $?
+````
+If `wget` isn't installed, it's command returns 1.
+
+If necessary install `wget`:
+````
+sudo apt install wget
+````
+
+For .tar.gz format execute the following command in terminal:
+````
+wget https://github.com/AntonieV/ExcelDiff/archive/refs/tags/latest.tar.gz -O - | tar -xz
+````
+
+### Alternatively: download and extract repository in .zip format: 
+Check if `unzip` and `wget` are installed:
+````
+which unzip; echo $?
+which wget; echo $?
+````
+If one of the tools isn't installed, it's command returns 1.
+If necessary install `wget` and/or `unzip`:
+````
+sudo apt install unzip
+sudo apt install wget
+````
+
+For .zip format execute the following command in terminal:
+````
+wget https://github.com/AntonieV/ExcelDiff/archive/refs/tags/latest.zip
+unzip latest.zip
+rm latest.zip
+````
+
+### Installation:
+````
+cd ExcelDiff
 pip3 install .
+````
 
-# package execution
-exceldiff -v -i ../test_table_1 ../test_table_2 -o ../diff
+## Compare two Excel/OpenOffice files:
 
-# local execution 
-python3 exceldiff/main.py -i ../test_table_1.ods ../test_table_2.ods -o ../diff
+### Input files:
+
+As input .xlsx or .ods files are accepted.
+
+### Output files:
+
+The differences are stored in the cells of an .xlsx results file. A difference
+is marked in corresponding cell with `>>>` annotation between the different 
+values: `[value of first input file] >>> [value of second input file]`
+
+![image info](./assets/Screenshot_ExcelDiff.png)
+
+
+In addition, they are stored with the corresponding cell localization 
+(sheet name, row and column) in a ExcelDiff_annotations file in 
+.txt format:
+
+![image info](./assets/Screenshot_ExcelDiff_annot.png)
+
+### Help:
+
+````
+exceldiff -h
+````
+![image info](./assets/Screenshot_help.png)
+
+### Example package execution:
+````
+exceldiff -v -i ../file_1.ods ../file_2.ods -o ../diff
+````
+![image info](./assets/Screenshot_ExcelDiff_stdout.png)
+
+### Example for local execution of the main method:
+
+````
+python3 exceldiff/main.py -v -i ../file_1.xlsx ../file_2.ods -o ../diff
+````
+
+## Uninstall package:
+````
+pip3 uninstall exceldiff
+````
+
 
--- exceldiff/main.py
+++ exceldiff/main.py
@@ -16,7 +16,7 @@ stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.DEBUG)
 stdout_handler.setFormatter(formatter)
 
-file_handler = RotatingFileHandler('logs.log', maxBytes=500*1024, backupCount=1)
+file_handler = RotatingFileHandler('logs.log', maxBytes=500 * 1024, backupCount=1)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
@@ -33,39 +33,43 @@ def compare_excel_files(excel_1, excel_2, out_dir):
     out_path = f'{out_dir}/'
     if exl_1.keys() == exl_2.keys():
         sheets = list(exl_1.keys())
-        for i in range(len(sheets)):
-            diff_annot += f"\tSheet '{sheets[i]}':\n"
-            if not exl_1[sheets[i]].equals(exl_2[sheets[i]]):
-                match_map = exl_1[sheets[i]] == exl_2[sheets[i]]
-                diff_rows, dif_cols = np.where(match_map == False)
-                for item in zip(diff_rows, dif_cols):
-                    col_position = exl_1[sheets[i]].columns.values[dif_cols]
+        res_exl_file = f'{out_path}ExcelDiff_{os.path.basename(excel_1)}_vs' \
+                       f'_{os.path.basename(excel_2)}.xlsx'
+        with pd.ExcelWriter(res_exl_file) as diff_writer:
+            for idx in range(len(sheets)):
+                logger.info(f"Analysing sheet '{sheets[idx]}'")
+                diff_annot += f"\tSheet '{sheets[idx]}':\n"
+                if not exl_1[sheets[idx]].equals(exl_2[sheets[idx]]):
+                    match_map = exl_1[sheets[idx]] == exl_2[sheets[idx]]
+                    diff_rows, dif_cols = np.where(match_map == False)
+                    for cell in zip(diff_rows, dif_cols):
+                        col_position = exl_1[sheets[idx]].columns.values[cell[1]]
+                        row_idx_shift = 1 if len(col_position) == 0 else 2
+                        if not (pd.isnull(exl_1[sheets[idx]].iloc[cell[0], cell[1]]) and
+                                pd.isnull(exl_1[sheets[idx]].iloc[cell[0], cell[1]])):
+                            exl_1_val = exl_1[sheets[idx]].iloc[cell[0], cell[1]]
+                            exl_2_val = exl_2[sheets[idx]].iloc[cell[0], cell[1]]
+                            diff_msg = f"In sheet '{sheets[idx]}' " \
+                                       f"[row: {cell[0] + row_idx_shift}, " \
+                                       f"col: " \
+                                       f"{col_position}]: " \
+                                       f"{exl_1_val} >>> " \
+                                       f"{exl_2_val}"
+                            logger.info(diff_msg)
+                            diff_annot += f"\t\t{diff_msg}\n"
+                            exl_1[sheets[idx]].iloc[cell[0], cell[1]] = f'{exl_1_val} ' \
+                                                                        f'>>> {exl_2_val}'
+                else:
+                    diff_annot += f"\t\tSheet '{sheets[idx]}' " \
+                                  f"does not show any differences.\n"
 
-                    row_idx_shift = 1 if len(col_position) == 0 else 2
-                    if not (pd.isnull(exl_1[sheets[i]].iloc[item[0], item[1]]) and
-                            pd.isnull(exl_1[sheets[i]].iloc[item[0], item[1]])):
-                        exl_1_val = exl_1[sheets[i]].iloc[item[0], item[1]]
-                        exl_2_val = exl_2[sheets[i]].iloc[item[0], item[1]]
-                        diff_msg = f"In sheet '{sheets[i]}' " \
-                                      f"[row: {item[0] + row_idx_shift}, " \
-                                      f"col: " \
-                                      f"{col_position[item[1]]}]: " \
-                                      f"{exl_1_val} >>> " \
-                                      f"{exl_2_val}"
-                        logger.info(diff_msg)
-                        diff_annot += f"\t\t{diff_msg}\n"
-                        exl_1[sheets[i]].iloc[item[0], item[1]] = f'{exl_1_val} ' \
-                                                                  f'>>> {exl_2_val}'
-                res_exl_file = f'ExcelDiff_sheet_{sheets[i]}.xlsx'
-                exl_1[sheets[i]].to_excel(out_path + res_exl_file, index=False,
-                                          header=True, encoding='utf-8')
-            else:
-                diff_annot += f"\t\tSheet '{sheets[i]}' " \
-                              f"does not show any differences.\n"
-        logger.info('ExcelDiff analysis finished!')
+                exl_1[sheets[idx]].to_excel(diff_writer, index=False,
+                                            header=True, encoding='utf-8',
+                                            sheet_name=sheets[idx])
+
     else:
         solution_msg = 'Please adjust the sheets of both ' \
-                      'files before.'
+                       'files before.'
         logger.error(f'For a differentiated comparison of the excel files, '
                      f'the sheets of both files must match in name and '
                      f'number. {solution_msg}')
@@ -77,14 +81,17 @@ def compare_excel_files(excel_1, excel_2, out_dir):
     with open(out_path + annot_file, 'w') as f:
         f.write(diff_annot)
 
+    logger.info('ExcelDiff analysis finished!')
+
 
 def main():
+    logging.getLogger().setLevel(logging.WARNING)
     parser = argparse.ArgumentParser(description="A tool to compare two excel files "
                                                  "with annotation of the differences.")
     parser.add_argument("-i", "--input-files",
                         nargs=2,
-                        help="Two paths to the Excel files to "
-                             "be compared with each other.",
+                        help="Two paths to the Excel files (.xlsx or .ods format) "
+                             "to be compared with each other.",
                         required=True)
     parser.add_argument("-o", "--out-dir",
                         nargs=1,
@@ -97,7 +104,6 @@ def main():
     input_files = args.input_files
     out_dir = ''.join(args.out_dir)
     verbose = args.verbose
-    # argument_list = sys.argv[1:]
 
     if "-h" in sys.argv[1:] or "--help" in sys.argv[1:]:
         parser.print_help()
@@ -118,7 +124,6 @@ def main():
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.WARNING)
     try:
         main()
     except Exception as e:
